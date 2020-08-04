2cbdcb7de801c1fe49f21d67ead780e02cb8e86e
up1 <somkiat.p@gmail.com>
2020-08-04 09:54:09 +0700

    Refactor code

--- src/main/java/CircularBuffer.java
+++ src/main/java/CircularBuffer.java
@@ -1,25 +1,29 @@
 public class CircularBuffer {
-    private int bufferSize = 10;
-    private String[] buffer;
+    private int bufferSize = 10; // 1.1
+    private final String[] buffer;
     private int readPointer;
     private int writePointer;
 
     // Default constructor
     public CircularBuffer() {
-        this.buffer = new String[10];
+        this(10);
+    }
+    // Overload constructor
+    public CircularBuffer(int size) {
+        this.bufferSize = size;
+        this.buffer = new String[bufferSize]; // 1.2
     }
 
     public boolean isEmpty() {
-        return bufferSize == 10;
+        return readPointer == writePointer;
     }
 
     public void writeData(String input) {
-        bufferSize--;
         this.buffer[writePointer++] = input;
     }
 
     public boolean isFull() {
-        return bufferSize == 0;
+        return writePointer >= bufferSize;
     }
 
     public String readData() {
--- src/test/java/CircularBufferTest.java
+++ src/test/java/CircularBufferTest.java
@@ -4,17 +4,22 @@ import static org.junit.Assert.*;
 
 public class CircularBufferTest {
 
+    CircularBuffer cb = new CircularBuffer(); // 1
+
+    @Test
+    public void create_new_buffer_with_size() {
+        cb = new CircularBuffer(5);
+    }
+
     @Test
     public void create_new_buffer_should_empty() {
-        CircularBuffer cb = new CircularBuffer();
         boolean result = cb.isEmpty();
         assertTrue("Buffer ไม่ว่างนะ", result);
     }
 
     @Test
     public void create_new_buffer_with_default_size_should_10() {
-        CircularBuffer cb = new CircularBuffer();
-        for(int i=0; i< 10; i++) {
+        for(int i=0; i< 10; i++) { // 2
             cb.writeData("A" + i);
         }
         boolean result = cb.isFull();
@@ -23,9 +28,9 @@ public class CircularBufferTest {
 
     @Test
     public void write_A_B_to_buffer_should_read_A_B_from_buffer() {
-        CircularBuffer cb = new CircularBuffer();
         cb.writeData("A");
         cb.writeData("B");
+
         assertEquals("A", cb.readData());
         assertEquals("B", cb.readData());
     }
